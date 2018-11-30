56c867a8ce7b18ef6205be3e661d58af1eab9f6a
Ozren Dabić <dabico@usi.ch>
2018-11-30 16:21:32 +0100

    Pushing Test Files
    Added some files from our Assignment 4 to use for testing

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-//const methodOverride = require('method-override');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const kleiDust = require('klei-dust');
