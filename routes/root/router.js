dc39b9468fdf52b722856eff4b514537994c34c5
hleni <hleni@usi.ch>
2018-12-05 16:57:32 +0100

    Added delete route

--- routes/root/router.js
+++ routes/root/router.js
@@ -51,7 +51,7 @@ router.get('/dir/*', function(req,res){
 			res.render('index', {dirList: directories, fileList: result});
 		} else {
 			res.type('application/json').json(result);
-		}	
+		}
 	}).catch(function(err){
 		res.status(500);
 		res.end("Internal Server Error!");
@@ -144,4 +144,63 @@ router.get('/download/*', function(req, res){
 		res.status(400);
 		res.end("File Could Not Be Downloaded!");
 	});
+});
+
+/* router.post('/upload', function(req,res) {
+	const entry = new Entries ({
+		isDir: req.body.isDir,
+		path: req.body.path,
+		name: req.body.name,
+		parent: req.body.parent,
+		extension: req.body.extension,
+		size: req.body.size,
+		timeCreated: req.body.timeCreated,
+		dateCreated: req.body.dateCreated,
+		tags: req.body.tags
+	});
+	entry.save(function(err, saved) {
+			if (!err) {
+					if (req.accepts("html")) {
+							res.status(201);
+							res.redirect("/");
+					}
+					else {
+							res.status(201).json(saved);
+					}
+			}
+			else {
+					res.status(400).end();
+			}
+	});
+});
+*/
+
+router.delete('/*', function(req, res) {
+    let path = req.originalUrl;
+
+    Entry.find({path: path}).then(function(found){
+        if (found == []) {
+            res.status(404).end("No Entries Found!");
+						return;
+        }
+        else {
+            return Entry.remove(found);
+        }
+		}).then(function(removed){
+				if(removed.isDir) {
+					fs.rmdirSync(path);
+				}
+				else{
+					fs.unlinkSync(path);
+				}
+				if (req.accepts("html")) {
+						res.status(204).redirect("/");
+				}
+				else {
+						res.json(removed);
+				}
+			}
+		}).catch(function(err){
+				res.status(500).end("Internal Server Error!");
+    });
 });
