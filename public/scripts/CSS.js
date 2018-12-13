359299ed644ed3a649dab977dfb08d27933f7b39
Davide Ciulla <davide.ciulla@ticino.com>
2018-12-13 17:50:02 +0100

    better indendation... sorry Salvatore :)

--- public/scripts/CSS.js
+++ public/scripts/CSS.js
@@ -1,42 +1,37 @@
 
-const css= () =>{
-                if (typeof(Storage) !== "undefined") {
-                    if (localStorage.getItem("css") == null){
-                        localStorage.setItem("css", "style-theme1.css");
-                    }
-                    var head  = document.getElementsByTagName('head')[0];
-                    var link  = document.createElement('link');
-                    link.rel  = 'stylesheet';
-                    link.type = 'text/css';
-                    link.href = "/css/"+localStorage.getItem("css");
-                    head.appendChild(link);
-
-                }else {
-                	console.log("error")
-                }
-            }
-        
+const css= () => {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.getItem("css") == null){
+            localStorage.setItem("css", "style-theme1.css");
+        }
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = "/css/"+localStorage.getItem("css");
+        head.appendChild(link);
+    } else {
+    	console.log("error")
+    }
+}
 
 document.getElementById("theme1")
-					.onclick = () => {
-   localStorage.setItem("css", "style-theme1.css");
-   css();
-};
-
-
-document.getElementById("theme2")
-					.onclick = () => {
-   localStorage.setItem("css", "style-theme2.css");
-   css();
-};
-
-
-document.getElementById("theme3")
-					.onclick = () => {
-    localStorage.setItem("css", "style-theme3.css");
+    .onclick = () => {
+        localStorage.setItem("css", "style-theme1.css");
     css();
 };
 
+document.getElementById("theme2")
+    .onclick = () => {
+        localStorage.setItem("css", "style-theme2.css");
+    css();
+};
+
+document.getElementById("theme3")
+    .onclick = () => {
+        localStorage.setItem("css", "style-theme3.css");
+    css();
+};
 
 document.getElementById("theme4")
 					.onclick = () => {
