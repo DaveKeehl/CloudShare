19aa331e2556d666767b9dbbb919d448710654d4
hleni <hleni@usi.ch>
2018-12-13 21:34:23 +0100

    Slight change in the main.js for the sockets
    Not complete, and need further work.

--- public/main.js
+++ public/main.js
@@ -15,18 +15,41 @@ function init() {
   });
 
   socket.on('entry.deleted', function(event) {
-    // not sure what goes here
+    const dom = document.getElementById(event._id);
+    if(dom) {
+      dom.outerHTML= "Entry deleted";
+    }
   });
 
   socket.on('entry.created', function(event) {
-    dust.render('entry/', event, function(err, out){
-      // something
+    dust.render('/', event, function(err, out){
+      const mainDom = document.getElementByTag("table");
+      let tr = document.createElement("tr");
+      mainDom.appendChild(tr);
+      tr.outerHTML = out;
     })
   });
 
-  socket.on('entry.moved', function(event) {
+  socket.on('entry.renamed', function(event) {
     // something
   });
-
-  // Need to work on further
 }
+
+// function addListener(form) {
+//       form.addEventListener("submit", (event) => {
+//           event.preventDefault();
+//       });
+//       const createDirButton = form.querySelector("#mkdirModal button[type="submit"]");
+//       createDirButton.addEventListener("click", createDirButtonListener);
+//       const createFileButton = form.querySelector("#uploadModal button[type="submit"]");
+//       createFileButton.addEventListener("click", createFileButtonListener);
+//       const deleteDirButton = form.querySelector("#deleteDirectoryModal button[type="submit"]");
+//       deleteDirButton.addEventListener("click", deleteDirButtonListener);
+//       const deleteFileButton = form.querySelector("#deleteFileModal button[type="submit"]");
+//       deleteFileButton.addEventListener("click", deleteFileButtonListener);
+//       const renameDirButton = form.querySelector("#renameDirectoryModal button[type="submit"]");
+//       renameDirButton.addEventListener("click", renameDirButtonListener);
+//       const renameFileButton = form.querySelector("#renameFileModal button[type="submit"]");
+//       renameFileButton.addEventListener("click", renameFileButtonListener);
+//
+// }
