3e98fa969df1834f17c7e37271903ece84abba2c
Davide Ciulla <davide.ciulla@ticino.com>
2018-12-13 17:14:01 +0100

    Changes to the GUI and to the file actions methods
    Deletion now works
    Taggins now works
    Upload has a new GUI

---
+++ public/js/utils.js
@@ -0,0 +1,50 @@
+const test = function() {
+    console.log(event.target.dataset.path);
+};
+
+const deleteFilePath = function() {
+    const eventPath = event.target.dataset.path;
+    const filePath = "/file/" + eventPath + "?_method=DELETE";
+    const deleteButton = document.getElementById("deleteFileButton");
+    deleteButton.formAction = filePath;
+};
+
+const deleteDirectoryPath = function() {
+    const eventPath = event.target.dataset.path;
+    const directoryPath = "/dir/" + eventPath + "?_method=DELETE";
+    console.log(directoryPath);
+    const deleteButton = document.getElementById("deleteDirectoryButton");
+    deleteButton.formAction = directoryPath;
+};
+
+const renameFilePath = function() {
+    const eventPath = event.target.dataset.path;
+    const filePath = "/file/" + eventPath + "?_method=PUT";
+    const deleteButton = document.getElementById("renameFileButton");
+    deleteButton.formAction = filePath;
+};
+
+const renameDirectoryPath = function() {
+    const eventPath = event.target.dataset.path;
+    const directoryPath = "/dir/" + eventPath + "?_method=PUT";
+    console.log(directoryPath);
+    const deleteButton = document.getElementById("renameDirectoryButton");
+    deleteButton.formAction = directoryPath;
+};
+
+const taggingPath = function() {
+    const eventPath = event.target.dataset.path;
+    console.log("eventPath:" + eventPath);
+    const addTagPath = "/tags/" + eventPath + "?_method=POST";
+    console.log("addTagPath:" + addTagPath);
+    const deleteTagPath = "/tags/" + eventPath + "?_method=DELETE";
+    console.log("deleteTagPath:" + deleteTagPath);
+    const clearTagPath = "/tags/" + eventPath + "?_method=PUT";
+    console.log("clearTagPath:" + clearTagPath);
+    const addTagButton = document.getElementById("addTag");
+    const deleteTagButton = document.getElementById("deleteTag");
+    const clearTagsButton = document.getElementById("clearTags");
+    addTagButton.formAction = addTagPath;
+    deleteTagButton.formAction = deleteTagPath;
+    clearTagsButton.formAction = clearTagPath;
+};
