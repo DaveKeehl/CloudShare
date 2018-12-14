72e0d60dd6fb1caa48c368d2edfaa286a373fc7c
Davide Ciulla <davide.ciulla@ticino.com>
2018-12-14 15:10:40 +0100

    Clipboard is halfway done

---
+++ public/js/clipboard.js
@@ -0,0 +1,52 @@
+$('.getDirectoryLink').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+});
+
+$('.getLink').tooltip({
+    trigger: 'click',
+    placement: 'bottom'
+});
+
+function setTooltipDirectory(message) {
+    $('.getDirectoryLink').tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+}
+
+function setTooltipFile(message) {
+    $('.getLink').tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show');
+}
+
+function hideTooltipDirectory() {
+    setTimeout(function() {
+        $('.getDirectoryLink').tooltip('hide');
+    }, 1000);
+}
+
+function hideTooltipFile() {
+    setTimeout(function() {
+        $('.getLink').tooltip('hide');
+    }, 1000);
+}
+
+var clipboardDir = new ClipboardJS('.getDirectoryLink');
+var clipboardFile = new ClipboardJS('.getLink');
+
+clipboardDir.on('success', function(e) {
+    alert("Copied!");
+});
+
+clipboardFile.on('success', function(e) {
+    alert("Copied!");
+});
+
+clipboardDir.on('error', function(e) {
+    alert("Failed!");
+});
+
+clipboardFile.on('error', function(e) {
+    alert("Failed!");
+});
